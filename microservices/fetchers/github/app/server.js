c0dbedfca6577af2be35927d85e2415dfa16a499
Radoslav Yankov <radoslavyankov@gmail.com>
2016-10-05 16:19:37 +0300

    added user fetching

--- microservices/fetchers/github/app/server.js
+++ microservices/fetchers/github/app/server.js
@@ -1,15 +1,45 @@
 'use strict';
 
 const express = require('express');
-const githubClient = require('github-client');
+const githubapi = require('github');
+
+var github = new githubapi({
+    // optional args
+    debug: true,
+    protocol: "https",
+    host: "api.github.com", // should be api.github.com for GitHub
+    pathPrefix: "", // for some GHEs; none for GitHub
+    headers: {
+        "user-agent": "HR_Bot" // GitHub is happy with a unique user agent
+    },
+    Promise: require('bluebird'),
+    followRedirects: false, // default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects
+    timeout: 5000
+});
 
 // Constants
 const PORT = 8080;
 
 // App
 const app = express();
+var sinceIndex = 1;
+
 app.get('/', function (req, res) {
-  res.send('Hello worldss :)))\n');
+  res.send('Hello world :)\n');
+});
+
+app.get('/fetch', function (req, res) {
+  github.users.getAll({
+    since:sinceIndex
+  }, function(err, res2) {
+        res.send(JSON.stringify(res2));
+        console.log("console: ", res2.meta.link + " asd");
+    // if (github.hasNextPage(res2)) {
+    //    github.getNextPage(res2, function(err, res3) {
+    //         res.send(JSON.stringify(res2));
+    //    });
+    // }
+    })
 });
 
 app.listen(PORT);
