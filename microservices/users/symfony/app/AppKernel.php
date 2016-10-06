f422aa10726b459c38d3e9fa87e537fbbe11b998
Jordan <jordantoen@gmail.com>
2016-10-06 23:03:57 +0100

    Changed: Move FOS to all environments.

--- microservices/users/symfony/app/AppKernel.php
+++ microservices/users/symfony/app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new FOS\RestBundle\FOSRestBundle()
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
@@ -23,7 +24,6 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-            $bundles[] = new FOS\RestBundle\FOSRestBundle();
         }
 
         return $bundles;
