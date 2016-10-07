724618d3466c0ec29d6d946a4ad2d66b0512e326
Radoslav Yankov <radoslavyankov@gmail.com>
2016-10-07 16:54:58 +0300

    added git users feature

--- microservices/fetchers/github/app/server.js
+++ microservices/fetchers/github/app/server.js
@@ -27,25 +27,83 @@ var github = new githubapi({
 });
 
 const PORT = 8080;
-
 const app = express();
+
+github.authenticate({
+    type: "oauth",
+    token: "58870b963599faddb13509af74b9b13a1ef63161"
+
+})
 
 app.get('/', function (req, res) {
   res.send('Hello world :)\n');
 });
 
 app.get('/fetch', function (req, res) {
+  var firstname, lastname, useremail, languages = [], fullname = [];
   github.users.getAll({
     since:sinceindex
   }, function(err, res2) {
-        //extracting the number from the full link
-        res.send(JSON.stringify(res2));
+        //extracting the number from the full link. also, some magic
+        for (var i = 0; i<1; i++){
+            firstname = lastname = useremail = "";
+            languages = fullname = [];
+            var full_user_link = res2[i].url;
+            var user = full_user_link.substring(full_user_link.lastIndexOf("/") + 1, full_user_link.length);
+            github.users.getForUser({ 
+                user: user
+             }, function(err, res3){
+                //extracting both names from the name string
+                if (res3.name!=null){
+                    fullname = res3.name.split(" ");
+                    firstname = fullname[0];
+                    if (fullname.length > 1){
+                        lastname = fullname[1];
+                    }
+                }
+                if (res3.email!=null){
+                    useremail = res3.email;
+                }
+                github.repos.getForUser({ 
+                    user: user
+
+                 }, function(err, res4){
+                    //getting the skills array from user's repos
+                    for (var j = 0; j<res4.length; j++){
+                        var shouldAdd = true;
+                        for (var k = 0; k<languages.length; k++){
+                            if (res4[j].language == languages[k] || res4[j].language == null) shouldAdd = false;
+                        }
+                        if (shouldAdd){
+                            languages.push(res4[j].language);
+                            shouldAdd = true;
+                        }
+                    }
+                    var skills = [{}];
+                    for (var j = 0; j<languages.length; j++){
+                        skills.push({
+                            'name': languages[j]
+                        })
+                    };
+
+                    var data = {
+                        'email' : useremail,
+                        'first_name' : firstname,
+                        'last_name' : lastname,
+                        'skills' : languages
+                    };
+                    // res.send(data);
+                    // console.log(data);
+
+                 });
+             });
+        }
+        //res.send(JSON.stringify(res2));
         var full_string = res2.meta.link;
         var start_pos = full_string.indexOf('=') + 1;
         var end_pos = full_string.indexOf('>',start_pos);
         var link = full_string.substring(start_pos,end_pos);
         sinceindex = parseInt(link);
-
         mongo.connect("mongodb://mongo:27017", function (err, db) {
             var index = {index: sinceindex};
             //when a new page is /fetch-ed the sinceindex increased. will fix so that it doesn't add new entries, only updating one
