a20b6df46a0141ac625ef7dd4b97c0b55d66b21d
Radoslav Yankov <radoslavyankov@gmail.com>
2016-10-10 17:57:26 +0300

    added posting to db. refactoring

--- microservices/fetchers/github/app/server.js
+++ microservices/fetchers/github/app/server.js
@@ -1,18 +1,24 @@
 'use strict';
 
+var request = require('request');
+var http = require('http');
 const express = require('express');
 const githubapi = require('github');
 const mongo = require('mongodb').MongoClient;
-var sinceindex = 0;
+
+var lastReachedIndex = 0;
+
+//mongo and github initialization
 mongo.connect("mongodb://mongo:27017", function (err, db) {
 
     //cursor for going through the database
     var cursor = db.collection('index-number').find();
     cursor.forEach(function(doc, err) {
-        //saving the last databse entry as sinceindex
-        sinceindex = doc.index;
+        //saving the last databse entry as lastReachedIndex
+        lastReachedIndex = doc.index;
     });
 });
+
 var github = new githubapi({
     debug: true,
     protocol: "https",
@@ -26,43 +32,38 @@ var github = new githubapi({
     timeout: 5000
 });
 
-const PORT = 8080;
-const app = express();
-
 github.authenticate({
     type: "oauth",
     token: "58870b963599faddb13509af74b9b13a1ef63161"
 
 })
 
-app.get('/', function (req, res) {
-  res.send('Hello world :)\n');
-});
+const PORT = 8080;
+const app = express();
 
 app.get('/fetch', function (req, res) {
-  var firstname, lastname, useremail, languages = [], fullname = [];
+  var firstname, lastname, email, languages = [];
   github.users.getAll({
-    since:sinceindex
+    since:lastReachedIndex
   }, function(err, res2) {
-        //extracting the number from the full link. also, some magic
-        for (var i = 0; i<1; i++){
-            firstname = lastname = useremail = "";
-            languages = fullname = [];
-            var full_user_link = res2[i].url;
+            firstname = lastname = email = "";
+            languages = [];
+            //extracting the index from the full link
+            var full_user_link = res2[1].url;
             var user = full_user_link.substring(full_user_link.lastIndexOf("/") + 1, full_user_link.length);
             github.users.getForUser({ 
                 user: user
              }, function(err, res3){
                 //extracting both names from the name string
                 if (res3.name!=null){
-                    fullname = res3.name.split(" ");
+                    var fullname = res3.name.split(" ");
                     firstname = fullname[0];
                     if (fullname.length > 1){
                         lastname = fullname[1];
                     }
                 }
                 if (res3.email!=null){
-                    useremail = res3.email;
+                    email = res3.email;
                 }
                 github.repos.getForUser({ 
                     user: user
@@ -70,55 +71,89 @@ app.get('/fetch', function (req, res) {
                  }, function(err, res4){
                     //getting the skills array from user's repos
                     for (var j = 0; j<res4.length; j++){
-                        var shouldAdd = true;
+                        var isNotDuplicate = true;
+                        //removes duplicates
                         for (var k = 0; k<languages.length; k++){
-                            if (res4[j].language == languages[k] || res4[j].language == null) shouldAdd = false;
+                            if (res4[j].language == languages[k] || res4[j].language == null) isNotDuplicate = false;
                         }
-                        if (shouldAdd){
+                        if (isNotDuplicate){
                             languages.push(res4[j].language);
-                            shouldAdd = true;
+                            isNotDuplicate = true;
                         }
                     }
-                    var skills = [{}];
+                    //proper formatting
+                    var skills = [];
                     for (var j = 0; j<languages.length; j++){
                         skills.push({
                             'name': languages[j]
                         })
                     };
 
-                    var data = {
-                        'email' : useremail,
-                        'first_name' : firstname,
-                        'last_name' : lastname,
-                        'skills' : languages
-                    };
-                    // res.send(data);
-                    // console.log(data);
+                    postToDb(res, firstname, lastname, email, skills);
 
                  });
              });
-        }
-        //res.send(JSON.stringify(res2));
-        var full_string = res2.meta.link;
-        var start_pos = full_string.indexOf('=') + 1;
-        var end_pos = full_string.indexOf('>',start_pos);
-        var link = full_string.substring(start_pos,end_pos);
-        sinceindex = parseInt(link);
-        mongo.connect("mongodb://mongo:27017", function (err, db) {
-            var index = {index: sinceindex};
-            //when a new page is /fetch-ed the sinceindex increased. will fix so that it doesn't add new entries, only updating one
-            db.collection('index-number').insertOne(index, function(err, result){
-            console.log("Item inserted ", sinceindex);
-
-            });
-
-        });
+        
+        saveIndexToMongo(res2);
 
     })
 });
 
+//posts the json to the database. doesn't work due to docker container linking
+function postToDb(res, firstname, lastname, useremail, skills){
+    var data = {
+                        'email' : useremail,
+                        'first_name' : firstname,
+                        'last_name' : lastname,
+                        'skills' : skills
+                    };
+                    res.send(data);
+
+                    var headers = {
+                        'Content-Type':     'application/json'
+                    }
+
+                    var options = {
+                    url: 'http://nginx/app_dev.php/users',
+                    method: 'POST',
+                    headers: headers,
+                    form: {
+                        'email' : useremail,
+                        'first_name' : firstname,
+                        'last_name' : lastname,
+                        'skills' : skills
+                    }
+                    }
+
+                    request(options, function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                    console.log(body)
+                    } else {
+                    }
+                    })
+}
+
+//saves the last reached index in mongodb.
+function saveIndexToMongo(res2){
+    var full_string = res2.meta.link;
+    var start_pos = full_string.indexOf('=') + 1;
+    var end_pos = full_string.indexOf('>',start_pos);
+    var link = full_string.substring(start_pos,end_pos);
+    lastReachedIndex = parseInt(link);
+    mongo.connect("mongodb://mongo:27017", function (err, db) {
+            var index = {index: lastReachedIndex};
+            //when a new page is /fetch-ed the lastReachedIndex increased. will fix so that it doesn't add new entries, only updating one
+            db.collection('index-number').insertOne(index, function(err, result){
+            console.log("Item inserted ", lastReachedIndex);
+
+            });
+
+        });
+}
+
+//resets the last reached index.
 app.get('/fetch_reset', function (req, res) {
-    sinceindex = 0;
+    lastReachedIndex = 0;
     res.send("fetch reset");
 });
 
